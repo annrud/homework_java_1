1f7d88626e17f8bb0b372146137bb91acb36c16f
annrud <rudina_anna88@mail.ru>
2023-01-30 10:53:26 +0800

    Fixed Exception Handling

--- src/main/java/homework2/FileAndLog.java
+++ src/main/java/homework2/FileAndLog.java
@@ -3,18 +3,14 @@ package homework2;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.logging.*;
 
 public class FileAndLog {
-    public static void main(String[] args) throws IOException{
-        Logger logger = Logger.getLogger(FileAndLog.class.getName());
-        FileHandler fh = new FileHandler("log.xml");
-        logger.addHandler(fh);
-        XMLFormatter xml = new XMLFormatter();
-        fh.setFormatter(xml);
-        logger.log(Level.WARNING, "Тестовое логирование 1");
-        logger.info("Тестовое логирование 2");
-        writeToFile(getString());
+    public static void main(String[] args){
+        try {
+                writeToFile(getString());
+            } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
     public static String getString() {
         String str = "TEST";
