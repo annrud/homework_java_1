8ed72f4f1918e8d2924b702a796e7e02ac4bf4d7
annrud <rudina_anna88@mail.ru>
2023-01-30 17:17:06 +0800

    Fixed Palindrome

--- src/main/java/homework2/Palindrome.java
+++ src/main/java/homework2/Palindrome.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
-        System.out.print(isPalindrome());
-    }
-    public static boolean isPalindrome() {
         Scanner in = new Scanner(System.in);
         System.out.println("Программа принимает на вход строку и " +
                 "определяет является ли строка палиндромом.");
         System.out.print("Введите строку: ");
         String str = in.nextLine().toLowerCase().replace(" ", "");
         in.close();
-        System.out.println(str);
+        System.out.print(isPalindrome(str));
+    }
+
+    public static boolean isPalindrome(String string) {
         int left = 0;
-        int right = str.length() - 1;
+        int right = string.length() - 1;
         while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
+            if (string.charAt(left) != string.charAt(right)) {
                 return false;
             }
             left++;
